1261ec6c8d57bf71ff8f05b5eacc54c8d784bf28
euhmeuh <jerome.martin@corp.ovh.com>
2017-10-19 13:54:59 +0200

    :tractor: chore(sidebar): make the product service code readable

--- client/app/components/product/product.service.js
+++ client/app/components/product/product.service.js
@@ -37,122 +37,130 @@ angular.module("services").service("Products", [
             return $q
                 .when(true)
                 .then(() => {
-                    if (!products) {
-                        if (requests.productsList === null) {
-                            requests.productsList = $q
-                                .all([
-                                    $http.get("/sws/products", {
-                                        serviceType: "aapi",
-                                        params: {
-                                            universe: constants.universe || "WEB",
-                                            worldPart: constants.target
-                                        }
-                                    }),
-                                    AllDom.getAllDoms(true),
-                                    Emails.getDelegatedEmails()
-                                ])
-                                .then((data) => {
-                                    const result = data[0];
-                                    const allDoms = data[1];
+                    if (products) {
+                        return products;
+                    }
 
-                                    if (result.status < 300) {
-                                        productsByType = result.data;
-
-                                        _.forEach(data[2], (email) => {
-                                            const splitted = email.split("@");
-                                            if (splitted.length >= 2) {
-                                                if (_.find(productsByType.emails, { name: splitted[1] }) == null) {
-                                                    const domain = splitted[1];
-                                                    productsByType.emails.push({
-                                                        displayName: domain,
-                                                        hasSubComponent: false,
-                                                        name: domain,
-                                                        type: "EMAIL_DELEGATE",
-                                                        delegate: true
-                                                    });
-                                                }
-                                            }
-                                        });
-
-                                        /* Exchange 25g */
-                                        if (productsByType && productsByType.platforms && productsByType.platforms.length) {
-                                            // 1. Remove all occurances and put them in other var
-                                            let exchangeOld = _.remove(productsByType.platforms, (a) => a.type === "EXCHANGE_OLD");
-                                            if (exchangeOld && exchangeOld.length) {
-                                                // 2. Merge all domain to an uniq array
-                                                exchangeOld = _.uniq(exchangeOld, (a) => a.name);
-
-                                                // 3. Push to products array
-                                                productsByType.platforms = productsByType.platforms.concat(exchangeOld);
-                                            }
-                                        }
-
-                                        if (!products) {
-                                            products = [];
-                                        }
-
-                                        let productDomains = [];
-                                        const allDomains = _.pluck(productsByType.domains, "name");
-                                        const allDomainsOnly = _.pluck(productsByType.domains.filter((x) => x.type === "DOMAIN"), "name");
-                                        const allZones = _.pluck(productsByType.domains.filter((x) => x.type === "ZONE" && allDomainsOnly.indexOf(x.name) === -1), "name");
-
-                                        if (allDoms && allDoms.length > 0) {
-                                            productsByType.allDoms = [];
-                                            return $q
-                                                .allSettled(
-                                                    allDoms.map((allDom) =>
-                                                        AllDom.getDomains(allDom).then(
-                                                            (domains) => {
-                                                                productDomains = productDomains.concat(allDomains.filter((d) => !~domains.indexOf(d)));
-
-                                                                productsByType.allDoms.push({
-                                                                    name: allDom,
-                                                                    displayName: allDom,
-                                                                    hasSubComponent: true,
-                                                                    type: "ALL_DOM",
-                                                                    subProducts: _.intersection(allDomains, domains).map((d) => ({
-                                                                        name: d,
-                                                                        displayName: d,
-                                                                        allDomName: allDom,
-                                                                        allDomZoneOnly: allZones.indexOf(d) !== -1,
-                                                                        type: allZones.indexOf(d) !== -1 ? "ZONE" : "ALL_DOM"
-                                                                    }))
-                                                                });
-                                                            },
-                                                            (err) => ({ error: err, allDom })
-                                                        )
-                                                    )
-                                                )
-                                                .then(() => {
-                                                    if (productsByType.allDoms.length > 0) {
-                                                        const d = productsByType.domains.filter((domain) => ~productDomains.indexOf(domain.name));
-
-                                                        productsByType.domains = productsByType.allDoms.concat(_.sortBy(d, (elt) => elt.name));
-                                                    }
-
-                                                    ["domains", "hostings", "exchanges", "sharepoints", "vps", "cdns", "emails", "licenseOffice", "allDoms", "emailPros"].forEach((type) => {
-                                                        products = products.concat(productsByType[type] || []);
-                                                    });
-
-                                                    return products;
-                                                });
-                                        }
-
-                                        ["domains", "hostings", "exchanges", "sharepoints", "vps", "cdns", "emails", "licenseOffice", "emailPros"].forEach((type) => {
-                                            products = products.concat(productsByType[type] || []);
-                                        });
-
-                                        return products;
-                                    }
-                                    return $q.reject(data);
-                                });
-                        }
-
+                    if (requests.productsList !== null) {
                         return requests.productsList;
                     }
-                    return products;
+
+                    requests.productsList = $q
+                        .all([
+                            $http.get("/sws/products", {
+                                serviceType: "aapi",
+                                params: {
+                                    universe: constants.universe || "WEB",
+                                    worldPart: constants.target
+                                }
+                            }),
+                            AllDom.getAllDoms(true),
+                            Emails.getDelegatedEmails()
+                        ])
+                        .then((data) => this.concatProducts(data));
                 })
                 .then(() => products, (reason) => $q.reject(reason));
+        };
+
+        this.concatProducts = function (data) {
+            const result = data[0];
+            const allDoms = data[1];
+
+            if (result.status >= 300) {
+                return $q.reject(data);
+            }
+
+            productsByType = result.data;
+
+            _.forEach(data[2], (email) => {
+                const splitted = email.split("@");
+                if (splitted.length >= 2) {
+                    if (_.find(productsByType.emails, { name: splitted[1] }) == null) {
+                        const domain = splitted[1];
+                        productsByType.emails.push({
+                            displayName: domain,
+                            hasSubComponent: false,
+                            name: domain,
+                            type: "EMAIL_DELEGATE",
+                            delegate: true
+                        });
+                    }
+                }
+            });
+
+            /* Exchange 25g */
+            if (productsByType && productsByType.platforms && productsByType.platforms.length) {
+                // 1. Remove all occurances and put them in other var
+                let exchangeOld = _.remove(productsByType.platforms, (a) => a.type === "EXCHANGE_OLD");
+                if (exchangeOld && exchangeOld.length) {
+                    // 2. Merge all domain to an uniq array
+                    exchangeOld = _.uniq(exchangeOld, (a) => a.name);
+
+                    // 3. Push to products array
+                    productsByType.platforms = productsByType.platforms.concat(exchangeOld);
+                }
+            }
+
+            if (!products) {
+                products = [];
+            }
+
+            if (!allDoms || allDoms.length <= 0) {
+                ["domains", "hostings", "exchanges", "sharepoints", "vps", "cdns", "emails", "licenseOffice", "emailPros"].forEach((type) => {
+                    products = products.concat(productsByType[type] || []);
+                });
+
+                return products;
+            }
+
+            return this.handleAllDoms(allDoms);
+        };
+
+        this.handleAllDoms = function (allDoms) {
+            let productDomains = [];
+            const allDomains = _.pluck(productsByType.domains, "name");
+            const allDomainsOnly = _.pluck(productsByType.domains.filter((x) => x.type === "DOMAIN"), "name");
+            const allZones = _.pluck(productsByType.domains.filter((x) => x.type === "ZONE" && allDomainsOnly.indexOf(x.name) === -1), "name");
+
+            productsByType.allDoms = [];
+            return $q
+                .allSettled(
+                    allDoms.map((allDom) =>
+                        AllDom.getDomains(allDom).then(
+                            (domains) => {
+                                productDomains = productDomains.concat(allDomains.filter((d) => !~domains.indexOf(d)));
+
+                                productsByType.allDoms.push({
+                                    name: allDom,
+                                    displayName: allDom,
+                                    hasSubComponent: true,
+                                    type: "ALL_DOM",
+                                    subProducts: _.intersection(allDomains, domains).map((d) => ({
+                                        name: d,
+                                        displayName: d,
+                                        allDomName: allDom,
+                                        allDomZoneOnly: allZones.indexOf(d) !== -1,
+                                        type: allZones.indexOf(d) !== -1 ? "ZONE" : "ALL_DOM"
+                                    }))
+                                });
+                            },
+                            (err) => ({ error: err, allDom })
+                        )
+                    )
+                )
+                .then(() => {
+                    if (productsByType.allDoms.length > 0) {
+                        const d = productsByType.domains.filter((domain) => ~productDomains.indexOf(domain.name));
+
+                        productsByType.domains = productsByType.allDoms.concat(_.sortBy(d, (elt) => elt.name));
+                    }
+
+                    ["domains", "hostings", "exchanges", "sharepoints", "vps", "cdns", "emails", "licenseOffice", "allDoms", "emailPros"].forEach((type) => {
+                        products = products.concat(productsByType[type] || []);
+                    });
+
+                    return products;
+                });
         };
 
         /*
@@ -174,8 +182,12 @@ angular.module("services").service("Products", [
                 };
             }
 
-            return this.getProducts(forceRefresh).then((productsList) => {
-                let productLength = productsList.length;
+            return this.getProducts(forceRefresh).then((prods) => {
+                const emptyProduct = {
+                    name: "",
+                    organization: "",
+                    type: ""
+                };
 
                 if ($.isEmpty(selectedProduct.name)) {
                     selectedProduct.name = $stateParams.productId ? $stateParams.productId : "";
@@ -183,34 +195,23 @@ angular.module("services").service("Products", [
                     selectedProduct.organization = $stateParams.organization ? $stateParams.organization : "";
 
                     if (selectedProduct.name === "") {
-                        return {
-                            name: "",
-                            organization: "",
-                            type: ""
-                        };
+                        return emptyProduct;
                     }
                 }
 
-                for (productLength; productLength--;) {
-                    if (productsList[productLength].name === selectedProduct.name && productsList[productLength].type === selectedProduct.type) {
-                        return productsList[productLength];
-                    } else if (productsList[productLength].hasSubComponent === true) {
-                        let i = 0;
-                        let found;
+                const found = _.find(prods, (prod) => {
+                    const check = (p) => p.name === selectedProduct.name && p.type === selectedProduct.type;
 
-                        while (!found && i < productsList[productLength].subProducts.length) {
-                            if (productsList[productLength].subProducts[i].name === selectedProduct.name && productsList[productLength].subProducts[i].type === selectedProduct.type) {
-                                found = productsList[productLength].subProducts[i];
-                            }
-                            i++;
-                        }
-                        if (found) {
-                            return found;
-                        }
+                    if (check(prod)) {
+                        return prod;
+                    } else if (prod.hasSubComponent) {
+                        return _.find(prod.subProducts, check);
                     }
-                }
 
-                return null;
+                    return null;
+                });
+
+                return found || emptyProduct;
             });
         };
 
