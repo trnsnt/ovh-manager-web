5c0a14f9fa52794515c891be6f768a33ac7d8c2e
Axel Peter <axel.peter@corp.ovh.com>
2018-01-26 13:11:00 +0100

    :tractor: chore(navbar): code review

--- client/app/components/user/session.service.js
+++ client/app/components/user/session.service.js
@@ -33,8 +33,7 @@ class SessionService {
         return languageChangingMenuObject;
     }
 
-    /* eslint-disable class-methods-use-this */
-    getProductsMenu (categoryName, products) {
+    static getProductsMenu (categoryName, products) {
         return _.map(products, (product) => ({
             title: product.displayName || product.name,
             state: `app.${categoryName}`,
@@ -43,7 +42,6 @@ class SessionService {
             }
         }));
     }
-    /* eslint-enable class-methods-use-this */
 
     getDomainsMenu (domains) {
         const domainsMenu = [{
@@ -84,51 +82,39 @@ class SessionService {
 
     getMicrosoftMenu (products) {
         // Exchange products
-        const exchangeLinks = [];
         const exchangeProducts = _.sortBy(products.exchanges, (elt) => angular.lowercase(elt.displayName || elt.name));
-
-        _.forEach(exchangeProducts, (elt) =>
-            exchangeLinks.push({
-                name: elt.name,
-                title: elt.displayName || elt.name,
-                state: elt.type === "EXCHANGE_PROVIDER" ? "app.microsoft.exchange.provider" : elt.type === "EXCHANGE_DEDICATED" ? "app.microsoft.exchange.dedicated" : "app.microsoft.exchange.hosted",
-                stateParams: {
-                    organization: elt.organization,
-                    productId: elt.name
-                }
-            })
-        );
+        const exchangeLinks = _.map(exchangeProducts, (elt) => ({
+            name: elt.name,
+            title: elt.displayName || elt.name,
+            state: elt.type === "EXCHANGE_PROVIDER" ? "app.microsoft.exchange.provider" : elt.type === "EXCHANGE_DEDICATED" ? "app.microsoft.exchange.dedicated" : "app.microsoft.exchange.hosted",
+            stateParams: {
+                organization: elt.organization,
+                productId: elt.name
+            }
+        }));
 
         // Office products
-        const officeLinks = [];
         const officeProducts = _.sortBy(products.licenseOffice, (elt) => angular.lowercase(elt.displayName || elt.name));
-
-        _.forEach(officeProducts, (elt) =>
-            officeLinks.push({
-                name: elt.name,
-                title: elt.displayName || elt.name,
-                state: "app.microsoft.office.product",
-                stateParams: {
-                    serviceName: elt.name
-                }
-            })
-        );
+        const officeLinks = _.map(officeProducts, (elt) => ({
+            name: elt.name,
+            title: elt.displayName || elt.name,
+            state: "app.microsoft.office.product",
+            stateParams: {
+                serviceName: elt.name
+            }
+        }));
 
         // SharePoint products
-        const sharepointLinks = [];
         const sharepointProducts = _.sortBy(products.sharepoints, (elt) => angular.lowercase(elt.displayName || elt.name));
-
-        _.forEach(sharepointProducts, (elt) =>
-            sharepointLinks.push({
-                name: elt.name,
-                title: elt.displayName || elt.name,
-                state: "app.microsoft.sharepoint.product",
-                stateParams: {
-                    exchangeId: elt.exchangeId,
-                    productId: elt.name
-                }
-            })
-        );
+        const sharepointLinks = _.map(sharepointProducts, (elt) => ({
+            name: elt.name,
+            title: elt.displayName || elt.name,
+            state: "app.microsoft.sharepoint.product",
+            stateParams: {
+                exchangeId: elt.exchangeId,
+                productId: elt.name
+            }
+        }));
 
         // Build Microsoft menu
         return [{
@@ -170,19 +156,19 @@ class SessionService {
         }, {
             name: "hosting",
             title: this.translator.tr("navigation_left_hosting"),
-            subLinks: this.getProductsMenu("hosting", hostingProducts)
+            subLinks: this.constructor.getProductsMenu("hosting", hostingProducts)
         }, {
             name: "private-database",
             title: this.translator.tr("navigation_left_database"),
-            subLinks: this.getProductsMenu("private-database", databaseProducts)
+            subLinks: this.constructor.getProductsMenu("private-database", databaseProducts)
         }, {
             name: "email-pro",
             title: this.translator.tr("navigation_left_emailPro"),
-            subLinks: this.getProductsMenu("email-pro", emailProProducts)
+            subLinks: this.constructor.getProductsMenu("email-pro", emailProProducts)
         }, {
             name: "email",
             title: this.translator.tr("navigation_left_email"),
-            subLinks: this.getProductsMenu("email.domain", emailProducts)
+            subLinks: this.constructor.getProductsMenu("email.domain", emailProducts)
         }, {
             name: "microsoft",
             title: this.translator.tr("navigation_left_microsoft"),
@@ -312,43 +298,38 @@ class SessionService {
         const managerUrls = this.constants.MANAGER_URLS;
         const managerNames = Object.keys(managerUrls);
 
-        return this.$q.all([
-            this.products.getProductsByType(),
-            this.user.getUser()
-        ]).then((result) => {
-            const products = result[0];
-            const currentUser = result[1];
+        return this.$q.all({
+            products: this.products.getProductsByType(),
+            user: this.user.getUser()
+        }).then((result) => ({
+            brand: {
+                title: this.translator.tr("universe_univers-portal_name"),
+                url: managerUrls.portal,
+                iconAlt: "OVH",
+                iconClass: "navbar-logo",
+                iconSrc: "assets/images/navbar/icon-logo-ovh.svg"
+            },
 
-            return {
-                brand: {
-                    title: this.translator.tr("universe_univers-portal_name"),
-                    url: managerUrls.portal,
-                    iconAlt: "OVH",
-                    iconClass: "navbar-logo",
-                    iconSrc: "assets/images/navbar/icon-logo-ovh.svg"
-                },
+            // Set Internal Links
+            internalLinks: this.getInternalLinks(result.user),
 
-                // Set Internal Links
-                internalLinks: this.getInternalLinks(currentUser),
+            // Set Manager Links
+            managerLinks: _.map(managerNames, (managerName) => {
+                const managerLink = {
+                    name: managerName,
+                    "class": managerName,
+                    title: this.translator.tr(`universe_univers-${managerName}_name`),
+                    url: managerUrls[managerName],
+                    isPrimary: ["partners", "labs"].indexOf(managerName) === -1
+                };
 
-                // Set Manager Links
-                managerLinks: _.map(managerNames, (managerName) => {
-                    const managerLink = {
-                        name: managerName,
-                        "class": managerName,
-                        title: this.translator.tr(`universe_univers-${managerName}_name`),
-                        url: managerUrls[managerName],
-                        isPrimary: ["partners", "labs"].indexOf(managerName) === -1
-                    };
+                if (managerName === currentUniverse) {
+                    managerLink.subLinks = this.getUniverseMenu(result.products);
+                }
 
-                    if (managerName === currentUniverse) {
-                        managerLink.subLinks = this.getUniverseMenu(products);
-                    }
-
-                    return managerLink;
-                })
-            };
-        });
+                return managerLink;
+            })
+        }));
     }
 }
 
